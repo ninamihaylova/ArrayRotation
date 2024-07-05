cee477d9de07cf2178c89f7d61f37b90b2e998e2
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 18:18:01 +0300

    Initial commit

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,28 @@
+﻿class ArrayRotation
+{
+    static void Main()
+    {
+        
+        int[] numbers = Console.ReadLine()
+            .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        
+        int rotations = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < rotations; i++)
+        {
+            int firstElement = numbers[0];
+            for (int j = 1; j < numbers.Length; j++)
+            {
+                numbers[j-1] = numbers[j];
+            }
+            numbers[numbers.Length - 1] = firstElement;
+        }
+
+        
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
+        
